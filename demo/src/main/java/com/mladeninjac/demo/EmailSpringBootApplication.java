0924022eadc28114eebb037b0b68501da5956275
Mladen Injac <injac.mladen@gmail.com>
2025-02-23 17:00:26 +0100

    push demo folder

---
+++ demo/src/main/java/com/mladeninjac/demo/EmailSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mladeninjac.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSpringBootApplication.class, args);
+	}
+
+}
